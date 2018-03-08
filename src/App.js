79de23a7ed550a84497b419a860d2a707c851de1
Alejok28 <alejo_k28@hotmail.com>
2018-03-07 22:46:57 -0500

    Adds guest to the list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      lastName: '',
+      guests: [],
+    };
+  }
+
+  handleChangeName(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+  handleChangeLastName(e) {
+    this.setState({
+      lastName: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    const name = this.state.name;
+    const lastName = this.state.lastName;
+    e.preventDefault();
+    this.setState({
+      name: '',
+      lastName: '',
+      guests: this.state.guests.concat({name: name, lastName: lastName})
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.name} onChange={this.handleChangeName.bind(this)}
+                />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.lastName} onChange={this.handleChangeLastName.bind(this)}/>
               </div>
 
               <div className="action">
@@ -30,6 +62,14 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
+                {this.state.guests.map((guest) => {
+                  return (
+                    <tr>
+                      <td>{guest.name}</td>
+                      <td>{guest.lastName}</td>
+                    </tr>
+                  )
+                })}
 
               </tbody>
             </table>
@@ -41,5 +81,3 @@ class App extends Component {
 }
 
 export default App
-
-
